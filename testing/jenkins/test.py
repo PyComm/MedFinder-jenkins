95d995c6fe615c97cc40f12f8fba0094308f3e9e
Cristian Bruna <cristian.bruna@sansano.usm.cl>
2023-07-04 22:59:17 -0400

    Without headless (back to normal)

--- testing/jenkins/test.py
+++ testing/jenkins/test.py
@@ -11,23 +11,22 @@ chrome_options = webdriver.ChromeOptions()
 
 driver = webdriver.Chrome(options=chrome_options)
 wait = WebDriverWait(driver, 10)
-
 try:
     # Test Case: Open MedicInfo
     driver.get("https://6943-186-78-252-47.ngrok-free.app/")
     driver.maximize_window()
-    
+
     # Esperar a que el enlace "Revisa Aquí" sea clicable
     visit_site_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(),'Visit Site')]")))
-    visit_site_link.click()
+    driver.execute_script("arguments[0].click();", visit_site_link)
 
     # Esperar a que el enlace "Revisa Aquí" sea clicable
-    revisa_aqui_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Revisa Aquí')]")))
-    revisa_aqui_link.click()
+    revisa_aqui_link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Revisa Aquí")))
+    driver.execute_script("arguments[0].click();", revisa_aqui_link)
 
     # Esperar a que el enlace "Información" sea clicable
-    informacion_link = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Información')]")))
-    informacion_link.click()
+    informacion_link = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Información")))
+    driver.execute_script("arguments[0].click();", informacion_link)
 
     # Esperar a que el párrafo con texto "Credencial" esté presente en la página
     credencial_paragraph = wait.until(EC.presence_of_element_located((By.XPATH, "//p[contains(text(), 'Credencial')]")))
@@ -35,7 +34,7 @@ try:
     # Capturar captura de pantalla
     driver.save_screenshot("screenshot.png")
 
-    time.sleep(5)
+    driver.implicitly_wait(5)
 
 finally:
     # Cerrar el navegador incluso si se produce una excepción
